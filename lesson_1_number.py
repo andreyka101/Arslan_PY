0066cb24db66c415cefa398ca57c8c69b90dcb68
andreyka101 <andrejkvasnin236@gmail.com>
2024-10-11 13:18:12 +0300

    first commit

---
+++ lesson_1_number.py
@@ -0,0 +1,61 @@
+# print(70)
+# print("hello world")
+# print('eyfgeyfgyegfy')
+# print('3')
+
+
+# num = 8
+# print(num)
+
+
+# hello_world = 7
+# helloWorld = 7
+# num_2 = 7
+
+
+# print(num_2 , num ,  "wertyu",34)
+
+
+# num_a_1 = 2 + 4 
+# num_a_2 = 2 - 4
+# num_a_3 = 2 / 4
+# num_a_4 = 2 * 4
+# num_a_5 = 2 ** 4 
+
+# print(num_a_1)
+# print(num_a_2)
+# print(num_a_3)
+# print(num_a_4)
+# print(num_a_5)
+
+# print(2 + 4 , 4)
+
+
+# num_a_6 = 2 * (4 + 3)
+# print(num_a_6)
+# print(2 * (4 + 3))
+
+
+# num_3 = 2
+# print(num_3)
+# num_3 = 7
+# print(num_3)
+# num_3 = num_3 + 2
+# print(num_3)
+# num_3 = num_3 - num_3
+# print(num_3)
+
+
+# num_int = 8
+# num_float = 8.7
+# num_str = "hi"
+# num_boot = False
+# num_null = None
+# print(num_int)
+# print(num_float)
+# print(num_str)
+# print(num_null)
+
+
+
+
