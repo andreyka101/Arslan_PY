700684947385f4fff25edb3bad53c39d17597950
andreyka101 <andrejkvasnin236@gmail.com>
2024-10-18 15:12:31 +0300

    feeffe

---
+++ lesson_2_string.py
@@ -0,0 +1,76 @@
+# print_r = 5
+# print(print_r)
+
+
+
+
+# num = 6.1
+# type() - возвращает тип переменной или значения
+# print(type(num))
+
+
+
+# преобразовываем тип из float в int
+# num_2 = 9.7
+# print(type(num_2))
+# num_2 = int(num_2)
+# print(num_2)
+# print(type(num_2))
+
+
+# преобразовываем тип из int в float
+# num_3 = 8
+# print(type(num_3))
+# num_3 = float(num_3)
+# print(num_3)
+# print(type(num_3))
+
+
+# складываем и умножаем строки 
+# print("4t" * 5)
+# print("hel" + "lo")
+
+
+# преобразовываем тип из str в int
+# num_4 = "8"
+# print(type(num_4))
+# num_4 = int(num_4)
+# print(num_4 * 3)
+# print(type(num_4))
+
+
+
+# input() - ввод текста
+# num_4 = 8
+# print(type(num_4))
+# num_4 = str(num_4)
+# print(num_4 * 3)
+# print(type(num_4))
+
+
+# num_5 = input("ввод--")
+# print("ok")
+# print(num_5)
+# print(type(num_5))
+
+
+# num_6 = input("ввод--")
+# print("ok")
+# num_6 = int(num_6)
+# print(num_6 * 3)
+# print(type(num_6))
+
+
+# num_6 = int(input("ввод--"))
+# print(num_6 * 3)
+# print(type(num_6))
+
+
+
+num_str = "qwerty"
+print(num_str)
+# вывод символа из строки
+print(num_str[0])
+num_str_2 = num_str[1] + num_str[4] + num_str[5]
+print(num_str_2)
+
